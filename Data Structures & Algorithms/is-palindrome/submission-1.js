062e1f9b2c763c674d4995ea7ad159caead17465
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2026-06-19 07:08:54 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isAlphanumeric(char){
+        return (char >= 'a' &&  char <= 'z') || (char >= 'A' && char <= 'Z') || (char >='0' && char <= '9')
+    }
+    isPalindrome(str) {
+        let i=0,  j = str.length-1;
+        while(i<j){
+            if(!this.isAlphanumeric(str[i])){ i++; continue};
+            if(!this.isAlphanumeric(str[j])) {j--; continue};
+            console.log("-->test",i,j,str[i],str[j])
+            if(str[i++]?.toLowerCase() != str[j--]?.toLowerCase()) return false;
+        }
+        return true
+    }
+}
